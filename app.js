d5dee689b4d1bceed6309c99ab0346e215ee7350
Kaowneaw <th.panya.bas@gmail.com>
2017-07-06 16:31:45 +0700

    passport

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const bodyPaser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/db');
+require('./config/passport')(passport);
+
+mongoose.connect(config.db);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' +config.db);
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Database error' + err);
+});
 
 const app = express();
 
@@ -19,6 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Body Parser Middleware
 app.use(bodyPaser.json());
 
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/users', users);
 
 //Index Route
@@ -30,23 +44,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
-
-
-
-// พิมพ์ 2 ออกมาทั้งสองครั้ง
-for(var i = 0; i < 2; i++) {
-  // เนื่องจากในจังหวะที่ console.log ทำงาน loop ได้วนไปจนครบแล้ว
-  // ทำให้ในขณะนั้่นค่า i มีค่าเป็น 2
-  // อย่าลืมว่า i ประกาศโดยใช้ var มันจึงผลักตัวเองออกจาก for
-  // หรือพูดง่ายๆคือ i นั้นเป็นตัวแปรตัวเดียวทุกครั้งที่วนลูปก็เพิ่มค่าใส่ตัวแปรเดิม
-  setTimeout(function() { console.log("var "+i) }, 100)
-}
-
-// ผลลัพธ์ได้ 0 และ 1
-// ใช้ let ประกาศตัวแปรทำให้ตัวแปรเป็น block-scoped
-// พูดง่ายๆคือ i จะเกิดขึ้นทุกครั้งที่วนลูป
-// และไม่ซ้ำกับ i ก่อนหน้า
-// เนื่องจากเป็น block-scoped มันจึงมีช่วงชีวิตอยู่แค่ใน {}
-for(let i = 0; i < 2; i++) {
-  setTimeout(function() { console.log("let "+i) }, 100)
-}
